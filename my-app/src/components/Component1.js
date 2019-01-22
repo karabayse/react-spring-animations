48b99728c78188170cb4437cba49c1320e9909b9
Kara Bayse <karabayse@gmail.com>
2019-01-22 15:28:15 -0600

    imported spring and added styling

--- my-app/src/components/Component1.js
+++ my-app/src/components/Component1.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {Spring} from 'react-spring';
 
 export default function Component1() {
-  return div (
-    <div>
+  return (
+    <div style={c1Style}>
       <h1>Component 1</h1>
       <p>First Paragraph</p>
     </div>
   )
+}
+
+const c1Style = {
+  background: '#36648B',
+  color: '#FFF',
+  padding: '1.5rem'
 }
