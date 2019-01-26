077a396797282d65a984a95eeaf402debb9e26ef
Kara Bayse <karabayse@gmail.com>
2019-01-26 14:41:12 -0600

    added Component2

---
+++ my-app/src/components/Component2.js
@@ -0,0 +1,13 @@
+import React {Component} from 'react'
+
+export class Component2 extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Component2 
