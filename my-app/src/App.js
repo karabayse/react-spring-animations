64900eea178009fbb32204a69e6bba07674c0c8a
Kara Bayse <karabayse@gmail.com>
2019-01-27 14:43:15 -0600

    added spring to component 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Component1 from './components/Component1';
+import Component2 from './components/Component2';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
           <Component1 />
+          <Component2 />
       </div>
     );
   }
--- my-app/src/components/Component2.js
+++ my-app/src/components/Component2.js
@@ -1,13 +1,30 @@
-import React {Component} from 'react'
+import React {Component} from 'react';
+import {Spring} from 'react-spring';
 
 export class Component2 extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+        <Spring
+          from={{opacity: 0}}
+          to={{opacity: 1}}
+        >
+          {props => (
+            <div style={props}>
+            <div style={c2Style}>
+              <h1>Component 2</h1>
+              <p>First Paragraph</p>
+            </div>
+            </div>
+          )}
+        </Spring>
     )
   }
 }
 
-export default Component2 
+const c2Style = {
+  background: 'slateblue',
+  color: '#FFF',
+  padding: '1.5rem'
+}
+
+export default Component2
