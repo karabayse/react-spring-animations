1010c93ee29665bb28e135dc18bc60ac05b34cb4
Kara Bayse <karabayse@gmail.com>
2019-01-30 15:54:44 -0600

    added component 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Component1 from './components/Component1';
 import Component2 from './components/Component2';
+import Component3 from './components/Component3';
 import './App.css';
 
 class App extends Component {
