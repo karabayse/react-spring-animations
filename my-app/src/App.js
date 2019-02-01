18c709e6f2852fd199687e86c9741f6ec219341c
Kara Bayse <karabayse@gmail.com>
2019-02-01 17:11:19 -0600

    added transition

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Transition, animate } from 'react-spring';
+import { Transition, animated } from 'react-spring';
 import Component1 from './components/Component1';
 import Component2 from './components/Component2';
 import Component3 from './components/Component3';
@@ -17,7 +17,19 @@ toggle = e => this.setState({showComponent3: !this.state.showComponent3});
       <div className="App">
           <Component1 />
           <Component2 toggle={this.toggle}/>
-          <Component3 />
+          <Transition
+            native
+            items={this.state.showComponent3}
+            from={{opacity: 0}}
+            enter={{opacity: 1}}
+            leave={{opacity: 0}}
+          >
+            {show => show && (props => (
+              <animated.div style={props}>
+                <Component3 />
+              </animated.div>
+            ))}
+          </Transition>
       </div>
     );
   }
